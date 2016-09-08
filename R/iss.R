14677ae8ee701edd84d15dd1aae054176f845205
config-i1 <ivan.svetunkov@gmail.com>
2016-09-08 23:51:33 +0100

    Updated iss. Now it really accepts smoothing parameters.

--- R/iss.R
+++ R/iss.R
@@ -76,28 +76,59 @@ intermittentMaker <- function(intermittent="n",...){
 }
 
 iss <- function(data, intermittent=c("none","fixed","croston","tsb"),
-                h=10, imodel=NULL, ipersistence=NULL){
+                h=10, holdout=FALSE, model=NULL, persistence=NULL){
 # Function estimates and returns mean and variance of probability for intermittent State-Space model based on the chosen method
     intermittent <- substring(intermittent[1],1,1);
     if(all(intermittent!=c("n","f","c","t"))){
         intermittent <- "f";
     }
-    y <- data;
-    obsInsample <- length(y);
+
+    obsInsample <- length(data) - holdout*h;
+    obsAll <- length(data) + (1 - holdout)*h;
+    y <- ts(data[1:obsInsample],frequency=frequency(data),start=start(data));
+
     ot <- abs((y!=0)*1);
+    otAll <- abs((data!=0)*1);
     iprob <- mean(ot);
     obsOnes <- sum(ot);
 # Sizes of demand
     yot <- matrix(y[y!=0],obsOnes,1);
 
+    if(!is.null(model)){
+        # If chosen model is "AAdN" or anything like that, we are taking the appropriate values
+        if(nchar(model)==4){
+            Etype <- substring(model,1,1);
+            Ttype <- substring(model,2,2);
+            Stype <- substring(model,4,4);
+            damped <- TRUE;
+            if(substring(model,3,3)!="d"){
+                message(paste0("You have defined a strange model: ",model));
+                sowhat(model);
+                model <- paste0(Etype,Ttype,"d",Stype);
+            }
+        }
+        else if(nchar(model)==3){
+            Etype <- substring(model,1,1);
+            Ttype <- substring(model,2,2);
+            Stype <- substring(model,3,3);
+            damped <- FALSE;
+        }
+    }
+    else{
+        model <- "MNN";
+        Etype <- "M";
+        Ttype <- "N";
+        Stype <- "N";
+    }
+
 #### Fixed probability ####
     if(intermittent=="f"){
-        pt <- ts(matrix(rep(iprob,obsInsample),obsInsample,1), start=start(data), frequency=frequency(data));
-        pt.for <- ts(rep(iprob,h), start=time(data)[obsInsample]+deltat(data), frequency=frequency(data));
-        errors <- ts(ot-iprob, start=start(data), frequency=frequency(data));
+        pt <- ts(matrix(rep(iprob,obsInsample),obsInsample,1), start=start(y), frequency=frequency(y));
+        pt.for <- ts(rep(iprob,h), start=time(y)[obsInsample]+deltat(y), frequency=frequency(y));
+        errors <- ts(ot-iprob, start=start(y), frequency=frequency(y));
 
-        model <- list(fitted=pt,forecast=pt.for,states=pt,variance=pt.for*(1-pt.for),
-                      likelihood=0,residuals=errors,C=c(0,iprob),actuals=ot)
+        output <- list(fitted=pt,forecast=pt.for,states=pt,variance=pt.for*(1-pt.for),
+                      likelihood=0,residuals=errors,C=c(0,iprob),actuals=otAll)
     }
 #### Croston's method ####
     else if(intermittent=="c"){
@@ -110,65 +141,64 @@ iss <- function(data, intermittent=c("none","fixed","croston","tsb"),
         zeroes <- diff(zeroes);
 # Number of intervals in Croston
         iyt <- matrix(zeroes,length(zeroes),1);
-        if(is.null(imodel)){
-            crostonModel <- es(iyt,"MNN",intervals=FALSE,int.w=0.95,silent=TRUE,h=h,ipersistence=ipersistence,intermittent="n");
-        }
-        else{
-            crostonModel <- es(iyt,model=imodel,intervals=FALSE,int.w=0.95,silent=TRUE,h=h,ipersistence=ipersistence);
-        }
+        crostonModel <- es(iyt,model=model,intervals=FALSE,int.w=0.95,silent=TRUE,h=h,persistence=persistence);
 
         zeroes[length(zeroes)] <- zeroes[length(zeroes)] - 1;
-        pt <- ts(rep((1-c(crostonModel$persistence)/2)/(crostonModel$fitted),zeroes),start=start(data),frequency=frequency(data));
-        pt.for <- ts((1-c(crostonModel$persistence)/2)/(crostonModel$forecast), start=time(data)[obsInsample]+deltat(data),frequency=frequency(data));
+        pt <- ts(rep((1-sum(crostonModel$persistence)/2)/(crostonModel$fitted),zeroes),start=start(y),frequency=frequency(y));
+        pt.for <- ts((1-sum(crostonModel$persistence)/2)/(crostonModel$forecast), start=time(y)[obsInsample]+deltat(y),frequency=frequency(y));
         likelihood <- - (crostonModel$ICs["AIC"]/2 - 3);
         C <- c(crostonModel$persistence,crostonModel$states[1,]);
         names(C) <- c(paste0("persistence ",c(1:length(crostonModel$persistence))),
                       paste0("state ",c(1:length(crostonModel$states[1,]))))
 
-        model <- list(fitted=pt,forecast=pt.for,states=1/crostonModel$states,variance=pt.for*(1-pt.for),
-                      likelihood=likelihood,residuals=crostonModel$residuals,C=C,actuals=ot);
+        output <- list(fitted=pt,forecast=pt.for,states=1/crostonModel$states,variance=pt.for*(1-pt.for),
+                      likelihood=likelihood,residuals=crostonModel$residuals,C=C,actuals=otAll);
     }
 #### TSB method ####
     else if(intermittent=="t"){
         ivt <- matrix(rep(iprob,obsInsample+1),obsInsample+1,1);
         iyt <- matrix(ot,obsInsample,1);
         modellags <- matw <- matF <- matrix(1,1,1);
-        vecg <- matrix(0.01,1,1);
-        errors <- matrix(NA,obsInsample,1);
-        iyt.fit <- matrix(NA,obsInsample,1);
 
-        if(!is.null(imodel)){
-# If chosen model is "AAdN" or anything like that, we are taking the appropriate values
-            if(nchar(imodel)==4){
-                Etype <- substring(imodel,1,1);
-                Ttype <- substring(imodel,2,2);
-                Stype <- substring(imodel,4,4);
-                damped <- TRUE;
-                if(substring(imodel,3,3)!="d"){
-                    message(paste0("You have defined a strange imodel: ",imodel));
-                    sowhat(imodel);
-                    imodel <- paste0(Etype,Ttype,"d",Stype);
-                }
+        if(!is.null(model)){
+            if(model!="MNN"){
+                warning("Sorry, but currently TSB can only use ETS(M,N,N) model.", call.=FALSE);
+                model <- "MNN";
+                Etype <- "M";
+                Ttype <- "N";
+                Stype <- "N";
             }
-            else if(nchar(imodel)==3){
-                Etype <- substring(imodel,1,1);
-                Ttype <- substring(imodel,2,2);
-                Stype <- substring(imodel,3,3);
-                damped <- FALSE;
+        }
+
+        if(!is.null(persistence)){
+            if(length(persistence)!=1){
+                warning("Only one smoothing parameter is currently supported for TSB. Using the first value.", call.=FALSE);
+                persistence <- persistence[1];
             }
+            persistenceEstimate <- FALSE;
+            vecg <- matrix(persistence,1,1);
+            A <- c(ivt[1]);
+            ALower <- c(1e-10);
+            AUpper <- c(1);
         }
         else{
-            Etype <- "M";
-            Ttype <- "N";
-            Stype <- "N";
+            persistenceEstimate <- TRUE;
+            vecg <- matrix(0.01,1,1);
+            A <- c(ivt[1],vecg[1]);
+            ALower <- c(1e-10,1e-10);
+            AUpper <- c(1,1);
         }
+
+        errors <- matrix(NA,obsInsample,1);
+        iyt.fit <- matrix(NA,obsInsample,1);
 
 #### CF for beta distribution ####
         CF <- function(C){
 
             fitting <- fitterwrap(ivt, matF, matw, iy_kappa, vecg,
-                                  modellags, "M", "N", "N", "o",
-                                  matrix(0,obsInsample,1), matrix(0,obsInsample+1,1), matrix(1,1,1), matrix(1,1,1), matrix(1,obsInsample,1));
+                                  modellags, Etype, Ttype, Stype, "o",
+                                  matrix(0,obsInsample,1), matrix(0,obsInsample+1,1),
+                                  matrix(1,1,1), matrix(1,1,1), matrix(1,obsInsample,1));
 
             iyt.fit <- fitting$yfit;
             errors <- fitting$errors;
@@ -180,12 +210,15 @@ iss <- function(data, intermittent=c("none","fixed","croston","tsb"),
 
 #### CF for initial and persistence ####
         CF2 <- function(C){
-            vecg[,] <- C[1];
-            ivt[1,] <- C[2];
+            ivt[1,] <- C[1];
+            if(persistenceEstimate){
+                vecg[,] <- C[2];
+            }
 
             fitting <- fitterwrap(ivt, matF, matw, iy_kappa, vecg,
-                                  modellags, "M", "N", "N", "o",
-                                  matrix(0,obsInsample,1), matrix(0,obsInsample+1,1), matrix(1,1,1), matrix(1,1,1), matrix(1,obsInsample,1));
+                                  modellags, Etype, Ttype, Stype, "o",
+                                  matrix(0,obsInsample,1), matrix(0,obsInsample+1,1),
+                                  matrix(1,1,1), matrix(1,1,1), matrix(1,obsInsample,1));
 
             iyt.fit <- fitting$yfit;
 
@@ -206,40 +239,49 @@ iss <- function(data, intermittent=c("none","fixed","croston","tsb"),
         C <- res$solution;
 
         # Another run, now to define persistence and initial
-        res <- nloptr(c(vecg[1],ivt[1]), CF2, lb=c(1e-10,1e-10), ub=c(1,1),
+        res <- nloptr(A, CF2, lb=ALower, ub=AUpper,
                       opts=list("algorithm"="NLOPT_LN_BOBYQA", "xtol_rel"=1e-6, "maxeval"=100));
-        C <- c(res$solution,C);
+
+        ivt[1,] <- res$solution[1];
+        if(persistenceEstimate){
+            vecg[,] <- res$solution[2];
+        }
+        if(persistenceEstimate){
+            C <- c(rev(res$solution),C);
+        }
+        else{
+            C <- c(persistence,res$solution,C);
+        }
 
         names(C) <- c("persistence","initial","shape1","shape2")
 
-        vecg[,] <- C[1];
-        ivt[1,] <- C[2];
         iy_kappa <- iyt*(1 - 2*kappa) + kappa;
         fitting <- fitterwrap(ivt, matF, matw, iy_kappa, vecg,
-                              modellags, "M", "N", "N", "o",
-                              matrix(0,obsInsample,1), matrix(0,obsInsample+1,1), matrix(1,1,1), matrix(1,1,1), matrix(1,obsInsample,1));
+                              modellags, Etype, Ttype, Stype, "o",
+                              matrix(0,obsInsample,1), matrix(0,obsInsample+1,1),
+                              matrix(1,1,1), matrix(1,1,1), matrix(1,obsInsample,1));
 
-        ivt <- ts(fitting$matvt,start=(time(data)[1] - deltat(data)),frequency=frequency(data));
-        iyt.fit <- ts(fitting$yfit,start=start(data),frequency=frequency(data));
-        errors <- ts(fitting$errors,start=start(data),frequency=frequency(data));
+        ivt <- ts(fitting$matvt,start=(time(y)[1] - deltat(y)),frequency=frequency(y));
+        iyt.fit <- ts(fitting$yfit,start=start(y),frequency=frequency(y));
+        errors <- ts(fitting$errors,start=start(y),frequency=frequency(y));
         iyt.for <- ts(rep(iyt.fit[obsInsample],h),
-                     start=time(data)[obsInsample]+deltat(data),frequency=frequency(data));
+                     start=time(y)[obsInsample]+deltat(y),frequency=frequency(y));
 
         # Correction so we can return from those iy_kappa values
         iyt.fit <- (iyt.fit - kappa) / (1 - 2*kappa);
         iyt.for <- (iyt.for - kappa) / (1 - 2*kappa);
 
-        model <- list(fitted=iyt.fit,states=ivt,forecast=iyt.for,variance=iyt.for*(1-iyt.for),
-                      likelihood=likelihood,residuals=errors,C=C,actuals=ot);
+        output <- list(fitted=iyt.fit,states=ivt,forecast=iyt.for,variance=iyt.for*(1-iyt.for),
+                      likelihood=likelihood,residuals=errors,C=C,actuals=otAll);
     }
 #### None ####
     else{
-        pt <- ts(rep(1,obsInsample),start=start(data),frequency=frequency(data));
-        pt.for <- ts(rep(1,h), start=time(data)[obsInsample]+deltat(data),frequency=frequency(data));
-        errors <- ts(rep(0,obsInsample), start=start(data), frequency=frequency(data));
-        model <- list(fitted=pt,states=pt,forecast=pt.for,variance=rep(0,h),
+        pt <- ts(rep(1,obsAll),start=start(y),frequency=frequency(y));
+        pt.for <- ts(rep(1,h), start=time(y)[obsInsample]+deltat(y),frequency=frequency(y));
+        errors <- ts(rep(0,obsInsample), start=start(y), frequency=frequency(y));
+        output <- list(fitted=pt,states=pt,forecast=pt.for,variance=rep(0,h),
                       likelihood=NULL,residuals=errors,C=c(0,1),actuals=pt);
     }
-    model$intermittent <- intermittent;
-    return(structure(model,class="iss"));
+    output$intermittent <- intermittent;
+    return(structure(output,class="iss"));
 }
