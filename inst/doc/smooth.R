34a2e70ff636e79981de8aa7d6cd135ea42612d5
config-i1 <ivan.svetunkov@gmail.com>
2016-09-16 12:39:37 +0100

    This is probably rubish.

--- inst/doc/smooth.R
+++
@@ -1,4 +0,0 @@
-## ----global_options, include=FALSE---------------------------------------
-knitr::opts_chunk$set(fig.width=6, fig.height=4, fig.path='Figs/', fig.show='hold',
-                      warning=FALSE, message=FALSE)
-
