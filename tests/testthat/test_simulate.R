eaa7a0989e7e9414f2ca813d3cfa40e1556fada1
config-i1 <ivan.svetunkov@gmail.com>
2016-10-25 00:22:32 +0100

    Testthat: sim.ssarima()

--- tests/testthat/test_simulate.R
+++ tests/testthat/test_simulate.R
@@ -20,3 +20,13 @@ testModel <- es(Mcomp::M3$N1984$x, "ANA", h=18, silent=TRUE)
 test_that("ETS(ANA) simulated from estimated model", {
     expect_match(simulate(testModel,nsim=10,seed=5,obs=100)$model, "ANA");
 })
+
+testModel <- auto.ssarima(Mcomp::M3$N1234$x, h=18, silent=TRUE)
+test_that("ARIMA(0,1,3) with drift simulated from estimated model", {
+    expect_match(simulate(testModel,nsim=10,seed=5,obs=100)$model, "(0,1,3)");
+})
+
+test_that("ARIMA(0,1,1) with intermittent data", {
+    expect_match(sim.ssarima(nsim=10,obs=100,iprob=0.2)$model, "iARIMA");
+})
+
