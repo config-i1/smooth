1bea75c04d65b56c6e73890f7918cbf1b5157297
Ivan Svetunkov <i.svetunkov@lancaster.ac.uk>
2016-11-07 12:13:11 +0000

    Solving initialisation problem for cases of backcasting.

--- R/es.R
+++ R/es.R
@@ -256,9 +256,10 @@ BasicInitialiserES <- function(...){
         if(initialType!="p"){
             initialstates <- matrix(NA,1,4);
 # "-1" is needed, so the level would correspond to the values before the in-sample
-            initialstates[1,2] <- cov(yot[1:min(12,obsNonzero)],c(1:min(12,obsNonzero)))/var(c(1:min(12,obsNonzero)));
-            initialstates[1,1] <- mean(yot[1:min(12,obsNonzero)]) - initialstates[1,2] * (mean(c(1:min(12,obsNonzero))) - 1);
-            initialstates[1,3] <- mean(yot[1:min(12,obsNonzero)]);
+            #min(max(12,datafreq),obsNonzero)
+            initialstates[1,2] <- cov(yot[1:obsNonzero],c(1:obsNonzero))/var(c(1:obsNonzero));
+            initialstates[1,1] <- mean(yot[1:obsNonzero]) - initialstates[1,2] * (mean(c(1:obsNonzero)));
+            initialstates[1,3] <- mean(yot[1:obsNonzero]);
             initialstates[1,4] <- 1;
         }
         else{
@@ -267,7 +268,7 @@ BasicInitialiserES <- function(...){
     }
     else{
         if(initialType!="p"){
-            initialstates <- matrix(rep(mean(yot[1:min(12,obsNonzero)]),4),nrow=1);
+            initialstates <- matrix(rep(mean(yot[1:obsNonzero]),4),nrow=1);
         }
         else{
             initialstates <- matrix(rep(initialValue,4),nrow=1);
