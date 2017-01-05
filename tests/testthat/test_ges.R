ae4131a7d0714acfe737a577540fccc6834c4bd8
Ivan Svetunkov <ivan@svetunkov.ru>
2017-01-05 17:56:19 +0300

    New testthat element + typo corrected

--- tests/testthat/test_ges.R
+++ tests/testthat/test_ges.R
@@ -33,3 +33,9 @@ test_that("Check exogenous variables for GES on N1457", {
     expect_equal(suppressWarnings(ges(y, h=18, holdout=TRUE, xreg=x, silent=TRUE)$model), testModel$model);
     expect_equal(suppressWarnings(forecast(testModel, h=18, holdout=FALSE)$model), testModel$model);
 })
+
+# Test selection of exogenous with GES
+testModel <- ges(y, h=18, holdout=TRUE, xreg=x, silent=TRUE, xregDo="select")
+test_that("Select exogenous variables for GES on N1457", {
+    expect_null(suppressWarnings(testModel$xreg));
+})
