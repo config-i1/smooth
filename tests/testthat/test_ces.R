d26ca220d29cec266f6f9a7fc2a60812926c7fbb
Ivan Svetunkov <i.svetunkov@lancaster.ac.uk>
2017-01-26 16:43:02 +0000

    Fixed a possible issue with as.numeric()

--- tests/testthat/test_ces.R
+++ tests/testthat/test_ces.R
@@ -14,7 +14,7 @@ test_that("Test on N1234$x, predefined CES", {
 # Test trace cost function for CES
 testModel <- ces(Mcomp::M3$N2568$x, seasonality="f", h=18, holdout=TRUE, silent=TRUE, intervals=TRUE)
 test_that("Test AICc of CES based on MSTFE on N2568$x", {
-    expect_equal(round(AICc(testModel),2), as.numeric(round(testModel$ICs["AICc"],2)));
+    expect_equal(as.numeric(round(AICc(testModel),2)), as.numeric(round(testModel$ICs["AICc"],2)));
 })
 
 # Test how different passed values are accepted by CES
