618b1ec62b5053c0339d7878fb9d2b9cea3cd487
Ivan Svetunkov <ivan@svetunkov.ru>
2017-06-13 23:41:04 +0100

    Method for the new class - vsmooth

---
+++ R/vmethods.R
@@ -0,0 +1,92 @@
+#' @export
+nobs.vsmooth <- function(object, ...){
+    return(nrow(object$fitted));
+}
+
+#' @export
+sigma.vsmooth <- function(object, ...){
+    return(sqrt(object$Sigma));
+}
+
+#### Extraction of parameters of models ####
+#' @export
+coef.vsmooth <- function(object, ...){
+
+    parameters <- object$coefficients;
+
+    return(parameters);
+}
+
+#' @export
+model.type.vsmooth <- function(object, ...){
+    model <- object$model;
+    modelType <- NA;
+    if(!is.null(model)){
+        if(gregexpr("VES",model)!=-1){
+            modelType <- substring(model,unlist(gregexpr("\\(",model))+1,unlist(gregexpr("\\)",model))-1);
+        }
+    }
+
+    return(modelType);
+}
+
+#### Prints of vsmooth ####
+#' @export
+print.vsmooth <- function(x, ...){
+    holdout <- any(!is.na(x$holdout));
+    intervals <- any(!is.na(x$lower));
+
+    if(all(holdout,intervals)){
+        insideintervals <- sum((x$holdout <= x$upper) & (x$holdout >= x$lower)) / length(x$forecast) * 100;
+    }
+    else{
+        insideintervals <- NULL;
+    }
+
+    intervalsType <- x$intervals;
+
+    cat(paste0("Time elapsed: ",round(as.numeric(x$timeelapsed,units="secs"),2)," seconds\n"));
+    cat(paste0("Model estimated: ",x$model,"\n"));
+    if(!is.null(x$nParam)){
+        if(x$nParam==1){
+            cat(paste0(x$nParam," parameter was estimated in the process\n"));
+        }
+        else{
+            cat(paste0(x$nParam," parameters were estimated in the process\n"));
+        }
+    }
+
+    cat(paste0("Cost function type: ",x$cfType))
+    if(!is.null(x$cfObjective)){
+        cat(paste0("; Cost function value: ",round(x$cfObjective,3),"\n"));
+    }
+    else{
+        cat("\n");
+    }
+
+    cat("\nInformation criteria:\n");
+    print(x$ICs);
+
+    if(intervals){
+        if(x$intervals=="p"){
+            intervalsType <- "parametric";
+        }
+        else if(x$intervals=="sp"){
+            intervalsType <- "semiparametric";
+        }
+        else if(x$intervals=="np"){
+            intervalsType <- "nonparametric";
+        }
+        else if(x$intervals=="a"){
+            intervalsType <- "asymmetric";
+        }
+        cat(paste0(x$level*100,"% ",intervalsType," prediction intervals were constructed\n"));
+    }
+
+}
+
+#### Summary of objects ####
+#' @export
+summary.vsmooth <- function(object, ...){
+    print(object);
+}
