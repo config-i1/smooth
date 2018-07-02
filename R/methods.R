49dffc2f8e11021fc16e58be320041c56b5a4496
Ivan Svetunkov <ivan@svetunkov.ru>
2018-07-01 20:21:39 -0700

    Shortened and optimised code of simulate.smooth

--- R/methods.R
+++ R/methods.R
@@ -1475,26 +1475,85 @@ print.iss <- function(x, ...){
 #### Simulate data using provided object ####
 #' @export
 simulate.smooth <- function(object, nsim=1, seed=NULL, obs=NULL, ...){
+    ellipsis <- list(...);
     if(is.null(obs)){
-        obs <- length(object$actuals);
+        obs <- nobs(object);
     }
     if(!is.null(seed)){
         set.seed(seed);
     }
 
+    # Start a list of arguments
+    args <- vector("list",0);
+
+    cfType <- object$cfType;
+    if(any(cfType==c("MAE","MAEh","TMAE","GTMAE","MACE"))){
+        randomizer <- "rlaplace";
+        if(!is.null(ellipsis$mu)){
+            args$mu <- ellipsis$mu;
+        }
+        else{
+            args$mu <- 0;
+        }
+
+        if(!is.null(ellipsis$b)){
+            args$b <- ellipsis$b;
+        }
+        else{
+            args$b <- mean(abs(residuals(object)));
+        }
+    }
+    else if(any(cfType==c("HAM","HAMh","THAM","GTHAM","CHAM"))){
+        randomizer <- "rs";
+        if(!is.null(ellipsis$mu)){
+            args$mu <- ellipsis$mu;
+        }
+        else{
+            args$mu <- 0;
+        }
+
+        if(!is.null(ellipsis$b)){
+            args$b <- ellipsis$b;
+        }
+        else{
+            args$b <- mean(sqrt(abs(residuals(object))));
+        }
+    }
+    else{
+        if(errorType(object)=="A"){
+            randomizer <- "rnorm";
+        }
+        else{
+            randomizer <- "rlnorm";
+        }
+        if(!is.null(ellipsis$mean)){
+            args$mean <- ellipsis$mean;
+        }
+        else{
+            args$mean <- 0;
+        }
+
+        if(!is.null(ellipsis$sd)){
+            args$sd <- ellipsis$sd;
+        }
+        else{
+            args$sd <- sigma(object);
+        }
+    }
+    args$randomizer <- randomizer;
+    args$frequency <- frequency(object$actuals);
+    args$obs <- obs;
+    args$nsim <- nsim;
+    args$initial <- object$initial;
+    args$iprob <- object$iprob[length(object$iprob)];
+
     if(gregexpr("ETS",object$model)!=-1){
         model <- modelType(object);
         if(any(unlist(gregexpr("C",model))==-1)){
-            if(substr(model,1,1)=="A"){
-                randomizer <- "rnorm";
-            }
-            else{
-                randomizer <- "rlnorm";
-            }
-            simulatedData <- sim.es(model=model, frequency=frequency(object$actuals), phi=object$phi,
-                                    persistence=object$persistence, initial=object$initial, initialSeason=object$initialSeason,
-                                    obs=obs,nsim=nsim,iprob=object$iprob[length(object$iprob)],
-                                    randomizer=randomizer,mean=0,sd=sqrt(object$s2),...);
+            args <- c(args,list(model=model, phi=object$phi, persistence=object$persistence,
+                                initialSeason=object$initialSeason));
+
+            simulatedData <- do.call("sim.es",args);
         }
         else{
             message("Sorry, but we cannot simulate data from combined model.");
@@ -1503,13 +1562,10 @@ simulate.smooth <- function(object, nsim=1, seed=NULL, obs=NULL, ...){
     }
     else if(gregexpr("ARIMA",object$model)!=-1){
         if(any(unlist(gregexpr("combine",object$model))==-1)){
-            orders <- orders(object);
-            lags <- lags(object);
-            randomizer <- "rnorm";
-            simulatedData <- sim.ssarima(orders=orders, lags=lags,
-                                         frequency=frequency(object$actuals), AR=object$AR, MA=object$MA, constant=object$constant,
-                                         initial=object$initial, obs=obs, nsim=nsim,
-                                         iprob=object$iprob[length(object$iprob)], randomizer=randomizer, mean=0, sd=sqrt(object$s2),...);
+            args <- c(args,list(orders=orders(object), lags=lags(object),
+                                AR=object$AR, MA=object$MA, constant=object$constant));
+
+            simulatedData <- do.call("sim.ssarima",args);
         }
         else{
             message("Sorry, but we cannot simulate data from combined model.");
@@ -1517,33 +1573,21 @@ simulate.smooth <- function(object, nsim=1, seed=NULL, obs=NULL, ...){
         }
     }
     else if(gregexpr("CES",object$model)!=-1){
-        model <- modelType(object);
-        initial <- object$initial;
-        randomizer <- "rnorm";
-        simulatedData <- sim.ces(seasonality=model,
-                                 frequency=frequency(object$actuals), A=object$A, B=object$B,
-                                 initial=object$initial, obs=obs, nsim=nsim,
-                                 iprob=object$iprob[length(object$iprob)], randomizer=randomizer, mean=0, sd=sqrt(object$s2),...);
+        args <- c(args,list(seasonality=modelType(object), A=object$A, B=object$B));
+
+        simulatedData <- do.call("sim.ces",args);
     }
     else if(gregexpr("GES",object$model)!=-1){
-        model <- object$model;
-        orders <- orders(object);
-        lags <- lags(object);
-        initial <- object$initial;
-        randomizer <- "rnorm";
-        simulatedData <- sim.ges(orders=orders, lags=lags, frequency=frequency(object$actuals), measurement=object$measurement,
-                                 transition=object$transition, persistence=object$persistence, initial=object$initial,
-                                 obs=obs, nsim=nsim,
-                                 iprob=object$iprob[length(object$iprob)], randomizer=randomizer, mean=0, sd=sigma(object),...);
+        args <- c(args,list(orders=orders(object), lags=lags(object),
+                            measurement=object$measurement, transition=object$transition,
+                            persistence=object$persistence));
 
+        simulatedData <- do.call("sim.ges",args);
     }
     else if(gregexpr("SMA",object$model)!=-1){
-        orders <- orders(object);
-        randomizer <- "rnorm";
-        simulatedData <- sim.sma(order=orders,
-                                 frequency=frequency(object$actuals),
-                                 initial=object$initial, obs=obs, nsim=nsim,
-                                 iprob=object$iprob[length(object$iprob)], randomizer=randomizer, mean=0, sd=sqrt(object$s2),...);
+        args <- c(args,list(order=orders(object)));
+
+        simulatedData <- do.call("sim.sma",args);
     }
     else{
         model <- substring(object$model,1,unlist(gregexpr("\\(",object$model))[1]-1);
