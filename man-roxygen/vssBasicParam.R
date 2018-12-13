abb25d8b0aa08e6e75fe603a759cdf079b3c5f61
Ivan Svetunkov <i.svetunkov@lancaster.ac.uk>
2018-12-13 18:12:54 +0000

    A more detailed explanation of parameters of ves

--- man-roxygen/vssBasicParam.R
+++ man-roxygen/vssBasicParam.R
@@ -3,54 +3,56 @@
 #' @param persistence Persistence matrix \eqn{G}, containing smoothing
 #' parameters. Can be:
 #' \itemize{
-#' \item \code{independent} - each series has its own smoothing parameters
+#' \item \code{"independent"} - each series has its own smoothing parameters
 #' and no interactions are modelled (all the other values in the matrix are set
 #' to zero);
-#' \item \code{dependent} - each series has its own smoothing parameters, but
+#' \item \code{"dependent"} - each series has its own smoothing parameters, but
 #' interactions between the series are modelled (the whole matrix is estimated);
-#' \item \code{group} each series has the same smoothing parameters for respective
+#' \item \code{"group"} each series has the same smoothing parameters for respective
 #' components (the values of smoothing parameters are repeated, all the other values
 #' in the matrix are set to zero).
 #' \item provided by user as a vector or as a matrix. The value is used by the model.
 #' }
+#' You can also use the first letter instead of writing the full word.
 #' @param transition Transition matrix \eqn{F}. Can be:
 #' \itemize{
-#' \item \code{independent} - each series has its own preset transition matrix
+#' \item \code{"independent"} - each series has its own preset transition matrix
 #' and no interactions are modelled (all the other values in the matrix are set
 #' to zero);
-#' \item \code{dependent} - each series has its own transition matrix, but
+#' \item \code{"dependent"} - each series has its own transition matrix, but
 #' interactions between the series are modelled (the whole matrix is estimated). The
 #' estimated model behaves similar to VAR in this case;
-#' \item \code{group} each series has the same transition matrix for respective
+#' \item \code{"group"} each series has the same transition matrix for respective
 #' components (the values are repeated, all the other values in the matrix are set to
 #' zero).
 #' \item provided by user as a vector or as a matrix. The value is used by the model.
 #' }
+#' You can also use the first letter instead of writing the full word.
 #' @param h Length of forecasting horizon.
 #' @param holdout If \code{TRUE}, holdout sample of size \code{h} is taken from
 #' the end of the data.
 #' @param ic The information criterion used in the model selection procedure.
-#' @param intervals Type of intervals to construct. NOT AVAILABLE YET!
+#' @param intervals Type of intervals to construct.
 #'
 #' This can be:
 #'
 #' \itemize{
-#' \item \code{none}, aka \code{n} - do not produce prediction
+#' \item \code{"none"}, aka \code{n} - do not produce prediction
 #' intervals.
-#' \item \code{conditional}, \code{c} - produces multidimensional elliptic
-#' intervals for each step ahead forecast.
-#' \item \code{unconditional}, \code{u} - produces separate bounds for each series
+#' \item \code{"conditional"}, \code{c} - produces multidimensional elliptic
+#' intervals for each step ahead forecast. NOT AVAILABLE YET!
+#' \item \code{"unconditional"}, \code{u} - produces separate bounds for each series
 #' based on ellipses for each step ahead. These bounds correspond to min and max
 #' values of the ellipse assuming that all the other series but one take values in
 #' the centre of the ellipse. This leads to less accurate estimates of bounds
-#' (wider intervals than needed), but these could still be useful.
-#' \item \code{independent}, \code{i} - produces intervals based on variances of
+#' (wider intervals than needed), but these could still be useful. NOT AVAILABLE YET!
+#' \item \code{"independent"}, \code{i} - produces intervals based on variances of
 #' each separate series. This does not take vector structure into account.
 #' }
 #' The parameter also accepts \code{TRUE} and \code{FALSE}. The former means that
-#' conditional intervals are constructed, while the latter is equivalent to
+#' the independent intervals are constructed, while the latter is equivalent to
 #' \code{none}.
-#'
+#' You can also use the first letter instead of writing the full word.
 #' @param level Confidence level. Defines width of prediction interval.
 #' @param cumulative If \code{TRUE}, then the cumulative forecast and prediction
 #' intervals are produced instead of the normal ones. This is useful for
