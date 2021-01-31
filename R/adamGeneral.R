9c216eac7aa2c72b294bd2ccdf9d4da76ac48dad
Ivan Svetunkov <ivan@svetunkov.ru>
2021-01-31 21:53:15 +0000

    Introduce logs for the NA treatment if data is positive only.

--- R/adamGeneral.R
+++ R/adamGeneral.R
@@ -124,8 +124,15 @@ parametersChecker <- function(data, model, lags, formulaProvided, orders, consta
         warning("Data contains NAs. The values will be ignored during the model construction.",call.=FALSE);
         X <- cbind(1,poly(c(1:obsAll),degree=min(max(trunc(obsAll/10),1),5)),
                    sinpi(matrix(c(1:obsAll)*rep(c(1:max(max(lags),10)),each=obsAll)/max(max(lags),10), ncol=max(max(lags),10))));
-        lmFit <- .lm.fit(X[!yNAValues,,drop=FALSE], matrix(y[!yNAValues],ncol=1));
-        y[yNAValues] <- (X %*% coef(lmFit))[yNAValues];
+        # If we deal with purely positive data, take logarithms to deal with multiplicative seasonality
+        if(any(y[!yNAValues]<=0)){
+            lmFit <- .lm.fit(X[!yNAValues,,drop=FALSE], matrix(y[!yNAValues],ncol=1));
+            y[yNAValues] <- (X %*% coef(lmFit))[yNAValues];
+        }
+        else{
+            lmFit <- .lm.fit(X[!yNAValues,,drop=FALSE], matrix(log(y[!yNAValues]),ncol=1));
+            y[yNAValues] <- exp(X %*% coef(lmFit))[yNAValues];
+        }
         if(!is.null(xregData)){
             xregData[yNAValues,responseName] <- y[yNAValues];
         }
