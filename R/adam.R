9f98548f43941b6a662eb8c1a7168a6e49290893
Ivan Svetunkov <ivan@svetunkov.ru>
2021-05-13 11:02:01 +0100

    A fix for logN and approximate prediction intervals

--- R/adam.R
+++ R/adam.R
@@ -7689,10 +7689,13 @@ forecast.adam <- function(object, h=10, newdata=NULL, occurrence=NULL,
                 }
             }
             else if(object$distribution=="dlnorm"){
-                yLower[] <- qlnorm(levelLow, sqrt(abs(1-vcovMulti))-1,
-                                   sqrt(2*1-sqrt(abs(1-vcovMulti))));
-                yUpper[] <- qlnorm(levelUp, sqrt(abs(1-vcovMulti))-1,
-                                   sqrt(2*1-sqrt(abs(1-vcovMulti))));
+                yLower[] <- qlnorm(levelLow, sqrt(abs(1-vcovMulti))-1, sqrt(vcovMulti));
+                yUpper[] <- qlnorm(levelUp, sqrt(abs(1-vcovMulti))-1, sqrt(vcovMulti));
+                # yLower[] <- qlnorm(levelLow, sqrt(abs(1-vcovMulti))-1,
+                #                    sqrt(2*1-sqrt(abs(1-vcovMulti))));
+                # yUpper[] <- qlnorm(levelUp, sqrt(abs(1-vcovMulti))-1,
+                #                    sqrt(2*1-sqrt(abs(1-vcovMulti))));
+                print(yLower)
                 if(Etype=="A"){
                     yLower[] <- (yLower-1)*yForecast;
                     yUpper[] <-(yUpper-1)*yForecast;
