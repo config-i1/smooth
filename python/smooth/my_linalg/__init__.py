18873e96324656165054b33ffe85784a8a9b8b3e
ltsaprounis <leonidas.tsap@gmail.com>
2023-12-20 21:03:18 +0000

    remove dummy my_linalg module

--- python/smooth/my_linalg/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Example python module for using carma and pybind11."""
-
-from ._my_linalg import example_attr, add, dot_product, arma_dot_product, array_sum
-
-
-__all__ = ["example_attr", "add", "dot_product", "arma_dot_product", "array_sum"]
