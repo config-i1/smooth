77180c6ba6dbefe5eca0c98a0dabfbab166eb55f
Ivan Svetunkov <ivan@svetunkov.com>
2025-11-05 15:41:03 +0000

    Fixes to make sure that this shit compiles

--- python/smooth/__init__.py
+++ python/smooth/__init__.py
@@ -1,10 +1,3 @@
 from smooth.adam_general.core.adam import Adam
 
 __all__ = ["Adam"]
-
-from .msdecompose import msdecompose, MsDecomposeResult
-
-__all__ = [
-    "msdecompose",
-    "MsDecomposeResult",
-]
