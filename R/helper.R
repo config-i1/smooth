a4df95833311583530c8c4735306865719df2812
Ivan Svetunkov <ivan@svetunkov.com>
2026-01-05 22:19:23 +0000

    Switch off the backcasted number of degrees of freedom

--- R/helper.R
+++ R/helper.R
@@ -54,7 +54,9 @@ calculateBackcastingDF <- function(profilesRecentTable, lagsModelAll,
     # na.rm is needed to avoid NaNs due to 0/0
     nStatesBackcasting <- sum(dfs1$profileRecent, na.rm=TRUE);
 
-    return(nStatesBackcasting);
+    # Switch off the backcasted number of degrees of freedom for now
+    return(0)
+    # return(nStatesBackcasting);
 }
 
 # The function calculates the discounted number of degrees of freedom for the model
